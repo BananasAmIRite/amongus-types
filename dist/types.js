8e910fb022775b920af4a82d8bedb6ec13ded11b
BananasAmIRite <jasony1230@gmail.com>
2022-08-21 12:07:57 -0400

    phased out usage of pre-commit to compile in favor of prepare script

--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GameRole = void 0;
-var GameRole;
-(function (GameRole) {
-    GameRole["IMPOSTER"] = "imposter";
-    GameRole["CREWMATES"] = "crewmate";
-})(GameRole = exports.GameRole || (exports.GameRole = {}));
